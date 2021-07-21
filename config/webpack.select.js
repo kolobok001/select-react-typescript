ff495cb375aee69e1ed15dd757dae30d1c48b3ed
Andrey <a.golushkov@pixlpark.com>
2021-07-21 16:57:03 +0700

    init

---
+++ config/webpack.select.js
@@ -0,0 +1,49 @@
+﻿const path = require("path");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = (env, argv) => {
+    const isProd = argv.mode === 'production';
+    const isAnal = argv.anal === 'true';
+    return {
+        entry: {
+            select: "./src/select.tsx"
+        },
+        mode: isProd ? 'production' : 'development',
+        devtool: isProd ? false : 'source-map',
+        performance: { hints: false },
+        output: {
+            filename: isProd ? '[name]-bundle.min.js' : '[name]-bundle.js',
+            path: path.resolve(__dirname, "./build/"),
+            libraryTarget: "umd",
+            pathinfo: false
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: ["raw-loader"],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: ["raw-loader"]
+                },
+                {
+                    test: /\.(js|ts)x?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                    },
+                }
+            ]
+        },
+        resolve: {
+            // Add `.ts` and `.tsx` as a resolvable extension.
+            extensions: [".ts", ".tsx", ".js", "jsx"],
+        },
+        plugins: [
+            new BundleAnalyzerPlugin({ analyzerMode: isAnal ? "server" : "disabled" })
+        ]
+    }
+    
+};
---
+++ src/index.tsx
@@ -0,0 +1,36 @@
+import React, { Component, createRef, ReactNode } from 'react'
+import classNames from 'classnames'
+
+export interface Option {
+
+}
+export interface Group {
+
+}
+export interface ReactDropdownProps {
+
+}
+export interface ReactDropdownState {
+
+}
+
+class Dropdown extends Component<ReactDropdownProps, ReactDropdownState> {
+
+    constructor(props: ReactDropdownProps) {
+        super(props)
+       
+    }
+
+
+    render() {
+
+        return (
+            <div >
+               
+            </div>
+        )
+    }
+}
+
+Dropdown.defaultProps = { baseClassName: 'dropdown' }
+export default Dropdown
